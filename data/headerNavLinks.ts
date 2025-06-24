6c1595adbaf621c6bebfb0387604bb3d22a69673
stdnoerr <stdnoerr@gmail.com>
2025-06-25 01:16:15 +0500

    initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/tags/writeup', title: 'Writeups' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
